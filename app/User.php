9c9e8159c9b5b5ebecb1baeac0732c30b58afc4d
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-03 17:22:38 +0100

    Install JWT auth

--- app/User.php
+++ app/User.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
 use App\SocialAccount;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -43,5 +45,25 @@ class User extends Authenticatable
     {
         # This user has many social media acounts
         return $this->hasMany(SocialAccount::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
